5b5e1e18eb3c662ceed8e729905b484c7777f03d
Izra Faturrahman <Frizz925@hotmail.com>
2019-11-23 19:26:11 +0700

    Implement zoom smoothing

--- src/map/index.ts
+++ src/map/index.ts
@@ -2,6 +2,11 @@ import mapImage from '@assets/floor_map_cf13.png';
 import merge from 'lodash/merge';
 import { Application, Container, Graphics, Sprite } from 'pixi.js';
 
+const SCALE_STEP = 0.04;
+const SCALE_EASE_STEP = 0.03;
+const VELOCITY_FRICTION = 0.84;
+const MAX_VELOCITY = 1.0;
+
 interface AppState {
   x: number;
   y: number;
@@ -10,24 +15,21 @@ interface AppState {
   offsetX: number;
   offsetY: number;
   scale: number;
-  originX: number;
-  originY: number;
-  originScale: number;
+  scaleVelocity: number;
+  scaleVelocityRAF?: number;
 }
 
 const renderContainer = document.getElementById('renderer');
 
-let appState: AppState = {
+const appState: AppState = {
   x: 0,
   y: 0,
   outerX: 0,
   outerY: 0,
   offsetX: 0,
   offsetY: 0,
-  scale: 1.1,
-  originX: 0,
-  originY: 0,
-  originScale: 1.1,
+  scale: 1.3,
+  scaleVelocity: 0,
 };
 
 const initApp = () => {
@@ -51,44 +53,68 @@ const initApp = () => {
   graphics.beginFill(0xff0000).drawRect(-5, -5, 10, 10);
   innerContainer.addChild(graphics);
 
-  const updateContainers = (state: AppState, prevState?: AppState) => {
-    if (!prevState) {
-      prevState = state;
-    }
-    const { x, y, outerX, outerY, offsetX, offsetY, scale } = state;
-    stage.position.set(x, y);
-    outerContainer.position.set(outerX, outerY);
-
-    const innerX = offsetX;
-    const innerY = offsetY;
-    graphics.position.set(innerX, innerY);
-    innerContainer.pivot.set(innerX, innerY);
-    innerContainer.position.set(innerX, innerY);
+  const updateContainers = (state: AppState) => {
+    const { x, y, outerX, outerY, offsetX, offsetY, scale, scaleVelocity } = state;
+    innerContainer.pivot.set(offsetX, offsetY);
+    innerContainer.position.set(offsetX, offsetY);
     innerContainer.scale.set(scale);
+    graphics.position.set(offsetX, offsetY);
+    outerContainer.position.set(outerX, outerY);
+    stage.position.set(x, y);
+
+    if (Math.abs(scaleVelocity) <= 1e-3) {
+      return;
+    }
+    appState.scaleVelocityRAF = window.requestAnimationFrame(() => {
+      const newScaleVelocity = scaleVelocity * VELOCITY_FRICTION;
+      const newScale = scale * (1.0 + SCALE_EASE_STEP * newScaleVelocity);
+      console.log(scaleVelocity, newScaleVelocity, scale, newScale);
+      updateState({
+        scale: newScale,
+        scaleVelocity: newScaleVelocity,
+      });
+    });
   };
   updateContainers(appState);
 
   const updateState = (state: Partial<AppState>) => {
-    const newState = merge({}, appState, state);
-    updateContainers(newState, appState);
-    appState = newState;
+    updateContainers(merge(appState, state));
   };
 
   const updateOffset = (evt: MouseEvent, delta: number, scale: number) => {
-    const deltaX = delta > 0 ? (evt.clientX - appState.offsetX - appState.outerX) / appState.scale : 0;
-    const deltaY = delta > 0 ? (evt.clientY - appState.offsetY - appState.outerY) / appState.scale : 0;
+    const deltaX =
+      delta >= 0 ? (evt.clientX - appState.offsetX - appState.outerX) / scale : 0;
+    const deltaY =
+      delta >= 0 ? (evt.clientY - appState.offsetY - appState.outerY) / scale : 0;
     const offsetX = appState.offsetX + deltaX;
     const offsetY = appState.offsetY + deltaY;
 
-    const originScale = appState.originScale;
-    const originDeltaScale = originScale - 1.0;
-    const deltaScale = appState.scale - 1.0;
-    const originX = appState.originX + deltaX * (deltaScale - originDeltaScale);
-    const originY = appState.originY + deltaY * (deltaScale - originDeltaScale);
-    const outerX = offsetX * originDeltaScale + originX;
-    const outerY = offsetY * originDeltaScale + originY;
+    const absDeltaScale = scale - 1.0;
+    const deltaScale = scale - appState.scale;
+    const outerX = appState.outerX + deltaX * absDeltaScale - deltaX * deltaScale;
+    const outerY = appState.outerY + deltaY * absDeltaScale - deltaY * deltaScale;
 
-    updateState({ outerX, outerY, offsetX, offsetY, scale, originX, originY, originScale });
+    const scaleVelocity =
+      Math.sign(appState.scaleVelocity * deltaScale) > 0
+        ? (delta > 0 ? Math.min : Math.max)(
+            appState.scaleVelocity + deltaScale,
+            MAX_VELOCITY * delta,
+          )
+        : deltaScale;
+    const scaleVelocityRAF = appState.scaleVelocityRAF;
+    if (scaleVelocityRAF) {
+      window.cancelAnimationFrame(scaleVelocityRAF);
+    }
+
+    updateState({
+      offsetX,
+      offsetY,
+      outerX,
+      outerY,
+      scale,
+      scaleVelocity,
+      scaleVelocityRAF: 0,
+    });
   };
 
   app.loader.add(mapImage).load(() => {
@@ -99,7 +125,7 @@ const initApp = () => {
 
   window.addEventListener('wheel', (evt: WheelEvent) => {
     const delta = -1 * Math.sign(evt.deltaY);
-    const amount = 0.1 * delta;
+    const amount = SCALE_STEP * delta;
     const scale = appState.scale * (1.0 + amount);
     updateOffset(evt, delta, scale);
   });
