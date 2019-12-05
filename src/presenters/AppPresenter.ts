c99709a1384f6940c7930c4e62297c3c3cc7564f
Izra Faturrahman <Frizz925@hotmail.com>
2019-12-06 02:13:04 +0700

    Allow card to stay tabbed when navigating pages

--- src/presenters/AppPresenter.ts
+++ src/presenters/AppPresenter.ts
@@ -42,7 +42,6 @@ export default class AppPresenter {
 
     pagePresenter.opened.subscribe(opened => {
       if (opened) {
-        cardPresenter.shown.next(false);
         cardPresenter.pulled.next(false);
         drawerPresenter.opened.next(false);
         searchPresenter.focused.next(false);
